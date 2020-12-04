f33e7aeb4089c5e2c41148b9299aab4f4a5a0068
Zero <Zeroto521@gmail.com>
2020-12-04 15:58:34 +0800

    :art: :bug: format codes & fix bug
    - :art: format codes
    - :bug: fix bug https://github.com/Flow-Launcher/Flow.Launcher/issues/155

--- HelloWorldPython/main.py
+++ HelloWorldPython/main.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from flow.launcher import Flow.Launcher
+from flowlauncher import FlowLauncher
+
 
 class HelloWorld(FlowLauncher):
 
     def query(self, query):
-        results = []
-        results.append({
-            "Title": "Hello World",
-            "SubTitle": "Query: {}".format(query),
-            "IcoPath":"Images/app.ico",
-            "ContextData": "ctxData"
-        })
-        return results
+        return [
+            {
+                "Title": "Hello World",
+                "SubTitle": "Query: {}".format(query),
+                "IcoPath": "Images/app.ico",
+                "ContextData": "ctxData",
+            }
+        ]
 
     def context_menu(self, data):
-        results = []
-        results.append({
-            "Title": "Context menu entry",
-            "SubTitle": "Data: {}".format(data),
-            "IcoPath":"Images/app.ico"
-        })
-        return results
+        return [
+            {
+                "Title": "Context menu entry",
+                "SubTitle": "Data: {}".format(data),
+                "IcoPath": "Images/app.ico",
+            }
+        ]
+
 
 if __name__ == "__main__":
     HelloWorld()
