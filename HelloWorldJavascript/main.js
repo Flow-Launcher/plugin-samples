5fdfb6e5254d555dac90794674a8b9fc1838d5d0
Zero <zeroto521@gmail.com>
2020-12-18 22:54:35 +0800

    simplify the script

--- HelloWorldJavascript/main.js
+++ HelloWorldJavascript/main.js
@@ -1,11 +1,11 @@
 const { method, parameters } = JSON.parse(process.argv.slice(2))
 
 if (method === "query") {
-  const result = [{
-    "Title": "Hello World Javascript",
-    "Subtitle": `Query: ${parameters}`,
-    "IcoPath": "Images\\app.png"
-  }]
-
-  console.log({ result })
+  console.log({
+    "result": [{
+      "Title": "Hello World Javascript",
+      "Subtitle": `Query: ${parameters}`,
+      "IcoPath": "Images\\app.png"
+    }]
+  })
 }
