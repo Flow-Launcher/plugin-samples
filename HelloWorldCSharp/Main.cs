9079fd24ec82fc91ca2ae5b97e25ab480b6fc26c
Jeremy <jeremy24wu@gmail.com>
2022-06-03 22:41:01 +1000

    fix HelloWorldCSharp project visibility  & add language support
    - HelloWorldCSharp project visibility was causing plugin load to fail
    - adds an example for language translation

--- HelloWorldCSharp/Main.cs
+++ HelloWorldCSharp/Main.cs
@@ -1,28 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Flow.Launcher.Plugin;
 
 namespace HelloWorldCSharp
 {
-    class Main : IPlugin
+    public class Main : IPlugin, IPluginI18n
     {
+        internal PluginInitContext Context;
+
         public List<Result> Query(Query query)
         {
             var result = new Result
             {
                 Title = "Hello World from CSharp",
                 SubTitle = $"Query: {query.Search}",
-                IcoPath = "app.png"
+                Action = c =>
+                {
+                    Context.API.ShowMsg(Context.API.GetTranslation("plugin_helloworldcsharp_greet_title"),
+                                            Context.API.GetTranslation("plugin_helloworldcsharp_greet_subtitle"));
+                    return true;
+                },
+                IcoPath = "Images/app.png"
             };
-            return new List<Result> {result};
+            return new List<Result> { result };
         }
 
         public void Init(PluginInitContext context)
         {
-            
+            Context = context;
+        }
+
+        public string GetTranslatedPluginTitle()
+        {
+            return Context.API.GetTranslation("plugin_helloworldcsharp_plugin_name");
+        }
+
+        public string GetTranslatedPluginDescription()
+        {
+            return Context.API.GetTranslation("plugin_helloworldcsharp_plugin_description");
         }
     }
 }
